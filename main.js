bc54694fbfd1d19c2306bef4dfd782234912e4f8
Victor Johnson <vicjohnson1213@gmail.com>
2018-04-06 22:10:15 -0500

    Updated the website to include some screenshots and limits the mods section to 3 mods.

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ $(function () {
     $.get('https://raw.githubusercontent.com/vicjohnson1213/SFMF/manifest/manifest.json')
         .then((res) => {
             let sfmf = JSON.parse(res);
-            console.log(sfmf);
 
-            sfmf.modlist.forEach(mod => {
-                $('#mod-list').append(createMod(mod));
-            });
+            $('#download-button').text(`Download ${sfmf.version}`)
+
+            sfmf.modlist
+                .filter(mod => mod.featured)
+                .slice(0,3)
+                .forEach(mod => {
+                    $('#mod-list').append(createMod(mod));
+                });
         });
 
     function getRandomColor() {
@@ -27,35 +31,48 @@ $(function () {
 
     function createMod(mod) {
         let col = $('<div>', {
-            class: 'siimple-grid-col siimple-grid-col--12'
-        });
-
-        let box = $('<div>', {
-            class: 'siimple-box mod-box'
+            class: 'siimple-grid-col siimple-grid-col--4 siimple-grid-col-sm--12'
         });
 
         let header = $('<div>', {
-            class: 'siimple-box mod-box-header'
-        }).addClass('siimple-box--' + getRandomColor());
-
-        let headerTitle = $('<div>', {
-            class: 'siimple-box-title',
+            class: 'siimple-h3',
             text: mod.name
         });
 
-        let body = $('<div>', {
-            class: 'mod-box-body',
+        let description = $('<div>', {
+            class: 'siimple-p',
             text: mod.description
         });
 
-        let version = $('<div>', {
-            class: 'mod-box-version',
-            text: mod.version
-        });
+        col.append(header);
+        col.append(description);
 
-        col.append(box.append(header.append(headerTitle)));
-        box.append(body);
-        box.append(version);
+        // let box = $('<div>', {
+        //     class: 'siimple-box mod-box'
+        // });
+        //
+        // let header = $('<div>', {
+        //     class: 'siimple-box mod-box-header'
+        // }).addClass('siimple-box--' + getRandomColor());
+        //
+        // let headerTitle = $('<div>', {
+        //     class: 'siimple-box-title',
+        //     text: mod.name
+        // });
+        //
+        // let body = $('<div>', {
+        //     class: 'mod-box-body',
+        //     text: mod.description
+        // });
+        //
+        // let version = $('<div>', {
+        //     class: 'mod-box-version',
+        //     text: mod.version
+        // });
+
+        // col.append(box.append(header.append(headerTitle)));
+        // box.append(body);
+        // box.append(version);
 
         return col;
     }
